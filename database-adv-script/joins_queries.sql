dbe8a9c5d0c6d63f4d8eadf531e10f47e5f534fb
Adetu Daniel <adetud680@gmail.com>
2025-11-06 12:40:58 +0100

    Fixed LEFT JOIN query to include properties without reviews

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -33,3 +33,13 @@ SELECT u.id AS user_id,
 FROM users u
 RIGHT JOIN bookings b
 ON u.id = b.user_id;
+
+-- Task 0: LEFT JOIN - retrieve all properties and their reviews, including those without reviews
+SELECT p.id AS property_id,
+       p.name AS property_name,
+       r.id AS review_id,
+       r.comment AS review_comment,
+       r.rating AS review_rating
+FROM properties p
+LEFT JOIN reviews r
+ON p.id = r.property_id;
